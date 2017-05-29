cf1bc0aece7ef8567a94b2d16df4c1b2aa5c8189
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-05-29 09:27:49 +0800

    s

--- util/extract_improvedfeatures.m
+++ util/extract_improvedfeatures.m
@@ -1,13 +1,11 @@
 % TODO: Change the paths and improved trajectory binary paths
-function [obj,trj,hog,hof,mbhx,mbhy] = extract_improvedfeatures(videofile,outfile)   
-   
-    
+function [obj,trj,hog,hof,mbhx,mbhy] = extract_improvedfeatures(videofile,outfile)    
     videofile = strrep(videofile, '&', '\&'); outfile = strrep(outfile, '&', '\&');
     videofile = strrep(videofile, '(', '\('); outfile = strrep(outfile, '(', '\(');
     videofile = strrep(videofile, ')', '\)'); outfile = strrep(outfile, ')', '\)');
     videofile = strrep(videofile, ';', '\;'); outfile = strrep(outfile, ';', '\;'); 
     [~,nameofvideo,~] = fileparts(videofile);
-    txtFile = fullfile('~/remote/Data/temp/tmpfiles',sprintf('%s-%1.6f',nameofvideo,tic())); % path of the temporary file
+    txtFile = fullfile('~/remote/Data/temp/tmpfiles',sprintf('%1.6f',tic())); % path of the temporary file
     % Here the path should be corrected
     system(sprintf('~/lib/improved_trajectory_release/release/DenseTrackStab %s > %s',videofile,txtFile));
     data = dlmread(txtFile);
